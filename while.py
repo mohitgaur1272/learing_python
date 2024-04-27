66470fe396233d3cc4b429d33bff13f351b819d9
mohitgaur1272 <montugaur1272@gmail.com>
2024-04-27 17:20:28 +0530

    new chnges

--- while.py
+++ while.py
@@ -7,8 +7,12 @@
 
 
 
-age = 28
+# age = 28
   
-# the test condition is always True 
-while age > 27: 
-    print('Infinite Loop')
+# # the test condition is always True 
+# while age > 27: 
+#     print('Infinite Loop')
+
+
+for i in range(0,800):
+    print(i)
