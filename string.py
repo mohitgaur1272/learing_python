3d521b9c26945de340d9a10d72eacce1af8edf89
mohitgaur1272 <montugaur1272@gmail.com>
2024-04-27 16:38:26 +0530

    bbjhcbjhbvbgv

---
+++ string.py
@@ -0,0 +1,11 @@
+a = "he's very good and is very nice person and is he very best of me"
+print([a.count('is')],
+      [a.count('and')] ,
+       [a.find('is')])
+print(a.center(100))
+print(a.index("person"))
+print(a.endswith("ver"))
+print(a.endswith("of me"))
+print(a.replace("person","humen"))
+print(a.title())
+print(a.find("good"))
---
+++ while.py
@@ -0,0 +1,14 @@
+# count = 2
+# while (count > 1) :
+#     print(count)
+#     count = count +  2
+
+
+
+
+
+age = 28
+  
+# the test condition is always True 
+while age > 27: 
+    print('Infinite Loop')
